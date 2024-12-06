5fdae6d124ce5a859032c568bbaaa0e8d65cd212
amiraryan1996 <amir.aryan.dv@gmail.com>
2024-12-06 11:20:32 +0330

    🐎 ci(dotenv): uninstalled dotenv
    created .env file manually at the cpanel.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-import dotenv from 'dotenv';
-dotenv.config()
-
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
@@ -25,20 +22,6 @@ const nextConfig = {
   },
   reactStrictMode: true,
   redirects,
-  env: {
-    HOST: process.env.HOST,
-    PORT: process.env.PORT,
-    DATABASE_URI: process.env.DATABASE_URI,
-    PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
-    PAYLOAD_PUBLIC_SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-    NEXT_PUBLIC_IS_LIVE: process.env.NEXT_PUBLIC_IS_LIVE,
-    PAYLOAD_PUBLIC_DRAFT_SECRET: process.env.PAYLOAD_PUBLIC_DRAFT_SECRET,
-    NEXT_PRIVATE_DRAFT_SECRET: process.env.NEXT_PRIVATE_DRAFT_SECRET,
-    REVALIDATION_KEY: process.env.REVALIDATION_KEY,
-    NEXT_PRIVATE_REVALIDATION_KEY: process.env.NEXT_PRIVATE_REVALIDATION_KEY,
-    WEBHOOKS_SECRET: process.env.WEBHOOKS_SECRET,
-  },
 }
 
 export default withPayload(nextConfig)
